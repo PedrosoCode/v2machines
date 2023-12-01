f629fb96eaa5fc84b5f1f865bfa0019ebf5132d9
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-12-01 07:56:27 -0300

    Início do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import './bootstrap/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="mt-5">Meu Layout Responsivo</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <p>Conteúdo da coluna 1</p>
+        </div>
+        <div className="col-md-6">
+          <p>Conteúdo da coluna 2</p>
+        </div>
+      </div>
     </div>
   );
 }
